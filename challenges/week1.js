73e153764a2d5843dded20a68e51137c6f80eb64
Katie Sheard <katiesheard@live.co.uk>
2020-02-15 22:44:06 +0000

    Passed 6/25 tests

--- challenges/week1.js
+++ challenges/week1.js
@@ -1,45 +1,47 @@
 function capitalize(word) {
-  if (word === undefined) throw new Error("word is required");
-  // Add your code here!
-}
+  if (word === undefined) throw new Error("word is required"); {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+};
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
-  if (lastName === undefined) throw new Error("lastName is required");
-  // Add your code here!
-}
+  if (lastName === undefined) throw new Error("lastName is required"); {
+    return firstName.charAt(0).toUpperCase() + "." + lastName.charAt(0).toUpperCase();
+  }
+};
 
 function addVAT(originalPrice, vatRate) {
-  if (originalPrice === undefined) throw new Error("originalPrice is requied");
-  if (vatRate === undefined) throw new Error("vatRate is required");
-  // Add your code here!
-}
+  if (originalPrice === undefined) throw new Error("originalPrice is required");
+  if (vatRate === undefined) throw new Error("vatRate is required"); {
+    return originalPrice + (vatRate * originalPrice)
+  }
+};
+const vatRate = 0.2
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
-}
+};
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  // Add your code here!
 }
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
-}
+  let splitString = word.split("");
+  let reverseArray = splitString.reverse();
+  let joinArray = reverseArray.join("");
+  return joinArray;
+};
 
 function reverseAllWords(words) {
-  if (words === undefined) throw new Error("words is required");
-  // Add your code here!
 }
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
-  // Add your code here!
-}
+};
 
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
