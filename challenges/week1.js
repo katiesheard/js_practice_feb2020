7927898973c8611dbb6d4cd41ca674ca47456e81
Katie Sheard <katiesheard@live.co.uk>
2020-02-19 13:35:52 +0000

    10/25 tests passed

--- challenges/week1.js
+++ challenges/week1.js
@@ -14,10 +14,9 @@ function generateInitials(firstName, lastName) {
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (vatRate === undefined) throw new Error("vatRate is required"); {
-    return originalPrice + (vatRate * originalPrice)
+    return originalPrice + (vatRate * originalPrice);
   }
 };
-const vatRate = 0.2
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
@@ -26,6 +25,16 @@ function getSalePrice(originalPrice, reduction) {
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
+  {
+    if (str.length % 2 === 0) {
+      let even = str.length / 2;
+      return str.slice(even - 1, even + 1);
+
+    } if (str.length % 2 !== 0) {
+      let odd = str.charAt((str.length / 2))
+      return odd;
+    }
+  }
 }
 
 function reverseWord(word) {
@@ -41,16 +50,22 @@ function reverseAllWords(words) {
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
+  if (users.type !== "Linux") {
+    return 0
+  }
 };
 
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
-  // Add your code here!
+  var total = 0;
+  for (i = 0; i < scores.length; i += 1) {
+    total += scores[i];
+  }
+  return total / scores.length;
 }
 
 function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
-  // Add your code here!
 }
 
 module.exports = {
